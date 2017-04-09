d34ab7f8e2ae565de6d121255919b27ab870fc6c
Paul Nakata <paul@pmn.org>
2017-04-09 13:28:15 -0700

    Return appropriate response

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const cite = require('citation')
 
 exports.CitationFinder = function(event, context, callback) {
     let input = event.text;
-    results = cite.find(input, { links: true });
-    callback(null, results);
+    let results = cite.find(input, { links: true });
+
+    
+    callback(null, {
+        statusCode: '200',
+        body: results,
+        headers: { 
+            'Content-Type': 'application/json', 
+        }
+    })
+    
 }
