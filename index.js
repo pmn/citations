ec7be6d4e8719d8c986b66636331a87a5503fcb3
Paul Nakata <paul@pmn.org>
2017-04-09 15:22:54 -0700

    Adding excerpts, calling lambda function directly now.

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ const cite = require('citation')
 
 exports.CitationFinder = function(event, context, callback) {
     let input = event.text;
-    let results = cite.find(input, { links: true });
-
-    
-    callback(null, {
-        statusCode: '200',
-        body: results,
-        headers: { 
-            'Content-Type': 'application/json', 
-        }
-    })
-    
+    let results = cite.find(input, { links: true, excerpt: 25 });
+    context.succeed(results);
 }
